9fbd002527a68b246eb10ae85b3aa8a5a1e60e6d
D3admaus <105454762+D3admaus@users.noreply.github.com>
2022-06-23 09:17:10 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var firstname='Reuben';
+var middlename='Gichuhi';
+var Height='1.89m';
+var age='21';
+var country='Kenya';
+Name=firstname+middlename;
+    document.write('Name:'+Name+'</br>'); 
+    document.write('Height:'+Height+'</br>');
+    document.write('Age:'+age+'</br>');
+    document.write('Country:'+country);
+ 
+
